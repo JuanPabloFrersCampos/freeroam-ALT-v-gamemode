9ffe5c9ae5e8988efb93b6acba4e6c74eb37e320
juanpablofrerscampos <juanpfrers@gmail.com>
2022-01-02 14:23:52 -0300

    Cleanup

--- resources/gamemode/server/index.js
+++ resources/gamemode/server/index.js
@@ -1,10 +1,12 @@
 /// <reference types="@altv/types-server" />
+
 //Database initialization
 import * as db from './database';
-const dataBaseCollections = [];
+//Collections to be initialized with node
+const dataBaseCollections = ['users'];
 new db.Database('mongodb://localhost:27017', 'freeoram-gm', dataBaseCollections);
 
-//"Static" modules
+//Other modules initialization
 import * as alt from 'alt-server';
 import './user-commands';
 import './main-events';
